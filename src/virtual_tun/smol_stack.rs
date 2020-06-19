02a3263fe46111722682a46ce73d83f9a8895761
lz <me@lucaszanella.com>
2020-06-19 20:45:45 +0000

    enhanced socket managing

--- src/virtual_tun/smol_stack.rs
+++ src/virtual_tun/smol_stack.rs
@@ -11,9 +11,11 @@ use smoltcp::storage::PacketMetadata;
 use smoltcp::wire::{
     IpAddress, IpCidr, IpEndpoint, IpProtocol, IpVersion, Ipv4Address, Ipv6Address,
 };
+use std::rc::Rc;
 use std::cell::RefCell;
 use std::collections::BTreeMap;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 
 pub enum SocketType {
     RAW_IPV4,
@@ -22,11 +24,29 @@ pub enum SocketType {
     UDP,
 }
 
-/*
-    
-*/
+pub struct Blob {
+    data: *mut u8, 
+    len: usize
+}
+
+pub struct SmolSocket<'a, 'b: 'a, 'c: 'a + 'b> {
+    pub tun_smol_stack: Rc<RefCell<TunSmolStack<'a, 'b, 'c>>>,
+    pub packets: VecDeque<Blob>
+}
+
+impl<'a, 'b: 'a, 'c: 'a + 'b> SmolSocket<'a, 'b, 'c> {
+    pub fn new(interface_name: String, tun_smol_stack: Rc<RefCell<TunSmolStack<'a, 'b, 'c>>>) -> Rc<RefCell<SmolSocket<'a, 'b, 'c>> {
+        SmolSocket {
+            tun_smol_stack: tun_smol_stack,
+            packets: VecDeque::new()
+        }
+    }
+}
+
 pub struct TunSmolStack<'a, 'b: 'a, 'c: 'a + 'b> {
     pub sockets: SocketSet<'a, 'b, 'c>,
+    current_key: usize,
+    socket_handles: HashMap<usize, SocketHandle>,
     device: Option<TunDevice>,
     ip_addrs: Option<std::vec::Vec<IpCidr>>,
     default_v4_gw: Option<Ipv4Address>,
@@ -42,6 +62,8 @@ impl<'a, 'b: 'a, 'c: 'a + 'b> TunSmolStack<'a, 'b, 'c> {
 
         Box::new(TunSmolStack {
             sockets: socket_set,
+            current_key: 0,
+            socket_handles: HashMap::new(),
             device: Some(device),
             ip_addrs: Some(ip_addrs),
             default_v4_gw: None,
@@ -50,21 +72,31 @@ impl<'a, 'b: 'a, 'c: 'a + 'b> TunSmolStack<'a, 'b, 'c> {
         })
     }
 
-    pub fn add_socket(&mut self, socket_type: SocketType) -> Box<SocketHandle> {
+    pub fn new_socket_handle_key(&mut self) -> usize {
+        //TODO: panic when usize is about to overflow
+        self.current_key += 1;
+        self.current_key
+    }
+
+    pub fn add_socket(&mut self, socket_type: SocketType) -> usize {
         match socket_type {
             SocketType::TCP => {
                 let rx_buffer = TcpSocketBuffer::new(vec![0; 1024]);
                 let tx_buffer = TcpSocketBuffer::new(vec![0; 1024]);
                 let socket = TcpSocket::new(rx_buffer, tx_buffer);
                 let handle = self.sockets.add(socket);
-                Box::new(handle)
+                let handke_key = self.new_socket_handle_key();
+                self.socket_handles.insert(handke_key, handle);
+                handke_key
             }
             SocketType::UDP => {
                 let rx_buffer = UdpSocketBuffer::new(Vec::new(), vec![0; 1024]);
                 let tx_buffer = UdpSocketBuffer::new(Vec::new(), vec![0; 1024]);
                 let socket = UdpSocket::new(rx_buffer, tx_buffer);
                 let handle = self.sockets.add(socket);
-                Box::new(handle)
+                let handke_key = self.new_socket_handle_key();
+                self.socket_handles.insert(handke_key, handle);
+                handke_key
             }
             /*
             SocketType::RAW_IPV4 => {
@@ -91,57 +123,69 @@ impl<'a, 'b: 'a, 'c: 'a + 'b> TunSmolStack<'a, 'b, 'c> {
 
     pub fn connect_ipv4(
         &mut self,
-        socket_handle: &SocketHandle,
+        socket_handle: usize,
         address: CIpv4Address,
         src_port: u16,
         dst_port: u16,
     ) -> u8 {
-        let mut socket = self.sockets.get::<TcpSocket>(*socket_handle);
-        let r = socket.connect(
-            (
-                Ipv4Address::new(
-                    address.address[0],
-                    address.address[1],
-                    address.address[2],
-                    address.address[3],
-                ),
-                dst_port,
-            ),
-            src_port,
-        );
-        match r {
-            Ok(()) => 0,
-            _ => 1,
+        let mut socket_handle_ = self.socket_handles.get(&socket_handle);
+        match socket_handle_ {
+            Some(socket_handle__) => {
+                let mut socket = self.sockets.get::<TcpSocket>(*socket_handle__);
+                let r = socket.connect(
+                    (
+                        Ipv4Address::new(
+                            address.address[0],
+                            address.address[1],
+                            address.address[2],
+                            address.address[3],
+                        ),
+                        dst_port,
+                    ),
+                    src_port,
+                );
+                match r {
+                    Ok(_) => 0,
+                    _ => 2,
+                }
+            }
+            None => 1
         }
     }
 
     pub fn connect_ipv6(
         &mut self,
-        socket_handle: &SocketHandle,
+        socket_handle: usize,
         address: CIpv6Address,
         src_port: u16,
         dst_port: u16,
     ) -> u8 {
-        let mut socket = self.sockets.get::<TcpSocket>(*socket_handle);
-        let r = socket.connect(
-            (
-                Ipv6Address::new(
-                    address.address[0],
-                    address.address[1],
-                    address.address[2],
-                    address.address[3],
-                    address.address[4],
-                    address.address[5],
-                    address.address[6],
-                    address.address[7],
-                ),
-                dst_port,
-            ),
-            src_port,
-        );
-        match r {
-            Ok(()) => 0,
-            _ => 1,
+        let mut socket_handle_ = self.socket_handles.get(&socket_handle);
+        match socket_handle_ {
+            Some(socket_handle__) => {
+                let mut socket = self.sockets.get::<TcpSocket>(*socket_handle__);
+                let r = socket.connect(
+                    (
+                        Ipv6Address::new(
+                            address.address[0],
+                            address.address[1],
+                            address.address[2],
+                            address.address[3],
+                            address.address[4],
+                            address.address[5],
+                            address.address[6],
+                            address.address[7],
+                        ),
+                        dst_port,
+                    ),
+                    src_port,
+                );
+                match r {
+                    Ok(_) => 0,
+                    _ => 2,
+                }
+            }
+            None => 1
         }
     }
 
